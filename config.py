e88c874c3b07b937b4ad0b6a0650e304e692ca50
yangshiling <1102566608@qq.com>
2023-08-26 16:22:42 +0800

    fix bugs and set dev devlopment

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-msg_tail = "\n [萝卜群号790121399] "
+msg_tail = "\n [test_dev] "
 api_token = ""
 tencent_token = ["",""]
-ws_port = 40019
+ws_port = 40018
 debug = False
 ws_addrs = {
-    # "nb2": "ws://ws.bot.isam.top:40008/onebot/v11/ws/",
+    "nb2": "ws://ws.bot.isam.top:40008/onebot/v11/ws/",
      "yunzai": "ws://127.0.0.1:2536/go-cqhttp",
-    # "yobot_remix": "ws://localhost:9222/ws/",
+    "yobot_remix": "ws://localhost:9222/ws/",
     #"qidian_nb" : "ws://localhost:40015/ws/"
     # "oldtr": "ws://127.0.0.1:9998/ws",
     
--- tools.py
+++ tools.py
@@ -5,13 +5,15 @@ time_start = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 def log(msg,level=3,log_file_name=None):
     # 保存到文件 daily.log
+    
+    msg = str(msg)
+    
     file_name = time_start if log_file_name is None else f"{log_file_name}_{time_start}"
+    now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if len(msg) < 1_000 and level <= log_level:
-        with open(f"log/{file_name}.log", "a",encoding="utf-8") as f:
-            now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f"[{now_time}]{msg}\n")
-            print(f"[{now_time}]{msg}")
-
+        print(f"[{now_time}]{msg}")
+    with open(f"F:/log/{file_name}.log", "a",encoding="utf-8") as f:
+        f.write(f"[{now_time}]{msg}\n")
 
 def get_ws_name(ws_addr):
     return [k for k, v in ws_addrs.items() if v == ws_addr][0]
@@ -23,18 +25,23 @@ def get_tail(ws_name):
 
 def msg_handle(ws_name, response):
     # return response
+    log(response)
     try:
         response = json.loads(response)
         if response.get("params"):
             if response["params"].get("message"): #if response["params"].get("message_type"):
                 log(f'debug {response["params"]["message"]}',3)
-                response["params"]["message"].append(
-                    {"type": "text", "data": {"text": get_tail(ws_name)}}
-                )
+                if isinstance(response["params"]["message"],str):
+                    response["params"]["message"] += get_tail(ws_name)
+                else:
+                    response["params"]["message"].append(
+                        {"type": "text", "data": {"text": get_tail(ws_name)}}
+                    )
             if response["params"].get("operation"):
                 response["params"]["operation"]["reply"] += get_tail(ws_name)
     except KeyError:
         log("3：Key Error of Response",1)
     except Exception as e:
-        log(e,1)
+        log(str(e)+str(response),1,log_file_name="e")
+        raise "a"
     return json.dumps(response)
